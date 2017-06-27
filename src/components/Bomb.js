3ac4ab81ed4485f76f2e740f52fa0a55ecf62cfc
tkoar <terrancekoar@gmail.com>
2017-06-27 10:12:45 -0400

    Done.

--- src/components/Bomb.js
+++ src/components/Bomb.js
@@ -1,1 +1,27 @@
 // Bomb Component Code Goes Here
+
+import React from 'react'
+
+export default class Bomb extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
+
+
+  render() {
+    if (this.state.secondsLeft === 0) {
+      return (
+        <div>Boom!</div>
+      )
+    } else {
+      return (
+        <div>{this.state.secondsLeft} seconds left before I go boom!</div>
+      )
+    }
+  }
+}
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Bomb from './components/Bomb';
 
 ReactDOM.render(
   <div>
-    <ImageSlider></ImageSlider>
+    <ImageSlider />
     <Bomb initialCount={120} />
   </div>,
   document.getElementById('root')
